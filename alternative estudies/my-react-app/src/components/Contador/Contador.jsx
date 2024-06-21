acc621e58ebbe352a3eaba3c354bc9349ccbe91e
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-21 19:38:09 -0300

    feat

--- alternative estudies/my-react-app/src/components/Contador/Contador.jsx
+++ alternative estudies/my-react-app/src/components/Contador/Contador.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import { React, useState} from 'react'
 import "./Contador.css"
 
 const Contador = props => {
+
+ const [contador, setContador] = useState(0);
+
+  const adicionar = () => {
+   setContador(contador + 1);
+  }
+
+
   return (
-      <div className="container-contador">
-        <div>1</div>
-      </div>
+    <div className="container-contador">
+      <div>{contador}</div>
+      <button onClick={adicionar} className="btn">Adicionar</button>
+    </div>
   )
 }
 
