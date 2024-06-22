40af1ac4e520a195a6c6b9a05460e02dd8fa4589
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-21 22:26:40 -0300

    feat

---
+++ cursoReact/primeiro-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu primeiro projeto</h1>
+      <p>Hello world!!</p>
+    </div>
+  );
+}
+
+export default App;
