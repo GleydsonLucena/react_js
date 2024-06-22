d8e8200c7c7027e92ee3f2c06160cc961b605e52
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-21 22:39:12 -0300

    feat

--- cursoReact/primeiro-projeto/src/App.js
+++ cursoReact/primeiro-projeto/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Gleydson';
+
+  const newName = name.toUpperCase();
+
+  const url = 'https://via.placeholder.com/200';
+
   return (
     <div className="App">
       <h1>Meu primeiro projeto</h1>
-      <p>Hello world!!</p>
+      <p>Olá, {newName}</p>
+      <img src={url} alt="minha imagem" />
     </div>
   );
 }
