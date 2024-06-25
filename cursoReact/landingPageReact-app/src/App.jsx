499897e3b4e9319017af1f837f0f0b832ed4d30a
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-25 19:42:58 -0300

    feat

---
+++ cursoReact/landingPageReact-app/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react';
+import './App.css'
+import Header from './components/Header/Header';
+
+
+function App() {
+
+  return (
+    <main className="mainContainer">
+      <Header/>
+    </main>
+  )
+}
+
+export default App
---
+++ cursoReact/landingPageReact-app/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="headerContainer">
+      <h2 className="logoText">LOGO</h2>
+      <nav className="navContainer">
+        <li>
+          <a href="#">Home</a>
+          <a href="#">Sobre</a>
+          <a href="#">Galeria</a>
+        </li>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
