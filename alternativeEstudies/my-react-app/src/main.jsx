0b8b86554e1ffbf688b228e9f4e9ce1915fd44b5
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-24 21:57:23 -0300

    Update main.jsx

--- alternativeEstudies/my-react-app/src/main.jsx
+++ alternativeEstudies/my-react-app/src/main.jsx
@@ -9,6 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <div className="main-container">
       <Home />
       <Contador />
-    </div>
-  </React.StrictMode>,gfdgd
+    </div>zd
+  </React.StrictMode>,
 )
