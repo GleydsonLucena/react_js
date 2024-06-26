6327b574a3d7b4b9c9e68c5c46d45f111a8f5cee
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-26 18:10:32 -0300

    dando inicio a landing page

---
+++ cursoReact/landinPage-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <header className="header-container">
+      <Header />
+    </header>
+  )
+}
+
+export default App
---
+++ cursoReact/landinPage-project/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="headerContent">
+      <h2>LOGO</h2>
+
+      <btnContact />
+    </div>
+  )
+}
+
+export default Header
