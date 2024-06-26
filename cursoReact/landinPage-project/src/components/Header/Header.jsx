5e508be45e523a2af01504a545103d0daf8bdf7d
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-06-26 18:23:30 -0300

    feat

--- cursoReact/landinPage-project/src/components/Header/Header.jsx
+++ cursoReact/landinPage-project/src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import "./Header.scss";
 
 const Header = () => {
   return (
     <div className="headerContent">
       <h2>LOGO</h2>
 
-      <btnContact />
+
     </div>
   )
 }
