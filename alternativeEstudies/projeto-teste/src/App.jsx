b838d11547bf67894a1cd34096e52d329857fed3
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-07-07 13:01:56 -0300

    .

---
+++ alternativeEstudies/projeto-teste/src/App.jsx
@@ -0,0 +1,20 @@
+import "./assets/styles/global.css";
+
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>LOGO</h1>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/sobre">Sobre</a></li>
+          <li><a href="/jogos">Jogos</a></li>
+        </ul>
+      </header>
+    </>
+  )
+}
+
+export default App
