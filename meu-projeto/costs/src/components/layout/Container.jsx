4ff5b1527731e5ba91b414841a22828673498ef4
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-08-03 13:03:19 -0300

    Update Container.jsx

--- meu-projeto/costs/src/components/layout/Container.jsx
+++ meu-projeto/costs/src/components/layout/Container.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import styles from "./module/Container.module.css";
+{
+    "window.zoomLevel": 1,
+    "editor.fontFamily": "JetBrains Mono",
+    "workbench.iconTheme": "symbols",
+    "workbench.colorTheme": "Min Dark",
+    "symbols.hidesExplorerArrows": false,
+    "editor.rulers": [80,120],
+    "editor.fontSize": 14,
+    "editor.renderLineHighlight": "gutter",
+    "editor.fontLigatures": true,
+    "workbench.editor.labelFormat": "short",
+    "explorer.compactFolders": false,
+    "editor.semanticHighlighting.enabled": false,
+    "breadcrumbs.enabled": false,
+    "workbench.activityBar.location": "bottom",
+    "editor.scrollbar.vertical": "hidden",
+    "editor.scrollbar.horizontal": "hidden",
+    "window.titleBarStyle": "native",
+    "apc.header": {
+        "height": 36
+    },
+    "apc.font.family": "Inter",
+    "apc.listRow": {
+        "height":24
 
-const Container = props => {
-  return (
-    <div
-      className={`${styles.container} ${styles[props.customClass]}`} >
-      {props.children}
-    </div>
-  )
+    },
+    "editor.tabSize": 2,
+    "editor.wordWrap": "on",
+    "editor.lineNumbers": "relative",
+    "editor.minimap.enabled": true,
+    "editor.lineHeight": 1.8,
+    "tabnine.experimentalAutoImports": true,
+    "chatgpt.lang": "pt-br",
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.formatOnType": true,
+    "notebook.defaultFormatter": "esbenp.prettier-vscode",
+    "notebook.formatOnSave.enabled": true,
+    "eslint.format.enable": true,
+    "html.format.indentHandlebars": true,
+    "window.customTitleBarVisibility": "auto",
+    "window.commandCenter": false,
+    "workbench.layoutControl.enabled": false,
+    "explorer.fileNesting.enabled": true,
+    "terminal.integrated.fontSize": 14,
+    "terminal.integrated.fontFamily": "JetBrainsMono Nerd Font",
+
 }
 
-export default Container
